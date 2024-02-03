f8163e3c6a086a9a8dd64209b5eb9bedbf7f9ab7
kostiantyn <kkononenko3@gmail.com>
2024-02-03 12:49:23 +0200

    created project, created app and created basic model

---
+++ custom_addons/hospital_service/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Hospital Service",
+    "description":"""Applications for hospital services for clients who value comfort and quality""",
+    "version": "1.0",
+    "author": "Kostiantyn Kononenko",
+    "category": "Human Resources",
+    "depends": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hospital_service_view.xml",
+        "views/menu_items.xml",
+    ],
+    "installable": True,
+    "application": True,
+    'license': 'LGPL-3',
+}
