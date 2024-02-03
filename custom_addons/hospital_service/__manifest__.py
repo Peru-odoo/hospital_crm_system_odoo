ab40cbfc48593ec2ca6f0635788b0733085df53a
kostiantyn <kkononenko3@gmail.com>
2024-02-03 13:57:25 +0200

    created patient view

--- custom_addons/hospital_service/__manifest__.py
+++ custom_addons/hospital_service/__manifest__.py
@@ -10,6 +10,7 @@
         "views/hospital_service_view.xml",
         "views/hospital_contact_person_view.xml",
         "views/hospital_doctor_view.xml",
+        "views/hospital_patient_view.xml",
         "views/menu_items.xml",
     ],
     "installable": True,
