d5c9028b588819d4afaf1598780a30cc1d433ae8
kostiantyn <kkononenko3@gmail.com>
2024-02-06 17:57:43 +0200

    сhanged the logic for selecting the date and time of reception by the user

--- custom_addons/hospital_service/models/hospital_doctor_schedule.py
+++ custom_addons/hospital_service/models/hospital_doctor_schedule.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class DoctorSchedule(models.Model):
@@ -18,11 +19,24 @@ class DoctorAvailabilitySlot(models.Model):
     start_time = fields.Float(string='Start Time', required=True)
     end_time = fields.Float(string='End Time', required=True)
 
+    @api.depends('start_time', 'end_time')
+    def _compute_name(self):
+        for slot in self:
+            start_time_str = "{:.2f}".format(slot.start_time)
+            end_time_str = "{:.2f}".format(slot.end_time)
+            slot.name = f"{start_time_str} - {end_time_str}"
+
+    name = fields.Char(string='Time Slot', compute='_compute_name', store=True)
+
+    @api.model
+    def name_get(self):
+        return [(slot.id, slot.name) for slot in self]
+
     @api.constrains('start_time', 'end_time')
     def _check_time_range(self):
         for slot in self:
             if slot.start_time >= slot.end_time:
-                raise ValueError("End time must be greater than start time")
+                raise ValidationError("End time must be greater than start time")
 
             same_day_slots = self.search([
                 ('schedule_id.appointment_date', '=', slot.schedule_id.appointment_date),
@@ -32,4 +46,4 @@ class DoctorAvailabilitySlot(models.Model):
             for existing_slot in same_day_slots:
                 if (slot.start_time < existing_slot.end_time and
                         slot.end_time > existing_slot.start_time):
-                    raise ValueError("Time slot overlaps with existing slots")
+                    raise ValidationError("Time slot overlaps with existing slots")
--- custom_addons/hospital_service/models/hospital_doctor_visit.py
+++ custom_addons/hospital_service/models/hospital_doctor_visit.py
@@ -5,9 +5,15 @@ class DoctorVisit(models.Model):
     _name = 'hospital.doctor_visit'
     _description = 'Doctor Visit'
 
+    time_slot = fields.Many2one(
+        'hospital.schedule.line',
+        string='Available Time',
+        domain="[('schedule_id.appointment_date', '=', appointment_date)]",
+        help='Select Time Slot',
+    )
     doctor = fields.Many2one('hospital.doctor', string='Doctor', required=True)
     patient = fields.Many2one('hospital.patient', string='Patient', required=True)
-    appointment_date = fields.Many2one('hospital.doctor_schedule', string='Doctor Appointment Date', required=True)
+    appointment_date = fields.Date(string='Appointment Date', required=True)
     research_ids = fields.Many2many('hospital.research', string='Research', copy=False)
     diagnosis_id = fields.Many2one('hospital.diagnosis', string='Diagnosis')
     recommendations = fields.Text(string='Recommendations')
